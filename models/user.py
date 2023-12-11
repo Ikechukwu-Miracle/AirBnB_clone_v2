d2694beb94af3bcdba19adc62c167861e230f987
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-12-11 02:54:22 +0000

    updated User, City, States, Placs

--- models/user.py
+++ models/user.py
@@ -2,6 +2,9 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
