0565842293c18e152e7b029d0fa222424953c19f
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-01-02 08:49:47 +0000

    fixed bash script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ This module contains the function do_pack that generates a .tgz archive
     from the contents of the web_static folder (fabric script) """
-
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """Generate a .tgz archive from web_static"""
