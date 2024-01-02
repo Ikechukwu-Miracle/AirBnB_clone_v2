08415775a9dd3f28d39e75ddf1ed599a1baacd3d
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-01-02 19:45:39 +0000

    created 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,7 @@ from datetime import datetime
 
 env.hosts = ["52.91.126.194", "100.26.170.64"]
 
+
 def do_deploy(archive_path):
     """
     Distributes an archive to your webservers
@@ -17,20 +18,20 @@ def do_deploy(archive_path):
 
     if not exists(archive_path):
         return False
-    
+
     fileName = archive_path.split('/')[-1]
-    no_extension = '/data/web_static/releases/' + "{}".format(fileName.split('.')[0])
+    no_ex = '/data/web_static/releases/' + "{}".format(fileName.split('.')[0])
     tmp = "/tmp/" + fileName
 
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_extension))
-        run("tar -xzf {} -C {}/".format(tmp, no_extension))
+        run("mkdir -p {}/".format(no_ex))
+        run("tar -xzf {} -C {}/".format(tmp, no_ex))
         run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_extension, no_extension))
-        run("rm -rf {}/web_static".format(no_extension))
+        run("mv {}/web_static/* {}/".format(no_ex, no_ex))
+        run("rm -rf {}/web_static".format(no_ex))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_extension))
+        run("ln -s {}/ /data/web_static/current".format(no_ex))
         return True
-    except:
+    except Exception as e:
         return False
